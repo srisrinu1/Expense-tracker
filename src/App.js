559b3b696f276b8f39f761218ab2fdbaead111d6
srisrinu1 <srinivasreddychalla133@gmail.com>
2021-11-06 22:34:12 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 import ExpenseItem from './components/ExpenseItem/ExpenseItem'
 
 function App() {
-  const expenses=[{expenseDate:new Date().toLocaleDateString(),expenseTitle:"MobileCharge",price:"Rs.48"}]
+  const expenses=[{expenseDate:new Date(),expenseTitle:"MobileCharge",price:"Rs.48"}]
   return (
     <div className="App">
       <ExpenseItem title={expenses[0].expenseTitle} price={expenses[0].price} date={expenses[0].expenseDate}/>
--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -6,9 +6,16 @@ export default function ExpenseItem(props) {
     // const date=currentDate.toLocaleDateString();
     // console.log(props)
     const {date,title,price}=props;
+    const month=date.toLocaleString("en-US",{month:"long"});
+    const day=date.toLocaleString("en-US",{day:"2-digit"});
+    const year=date.toLocaleString("en-US",{year:"numeric"})
     return (
         <div className="expense-item">
-            <div>{date}</div>
+          <div className="">
+            <div>{month}</div>
+            <div>{day}</div>
+            <div>{year}</div>
+            </div>
             <div className="expense-item__description">
             <h2>{title}</h2>
             <div className="expense-item__price">{price}</div>
